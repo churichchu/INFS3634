a7dcfd281656ff492c829d283b90cf4b1d07e9df
Richard Zhu <churichchu@yahoo.com>
2018-10-19 13:35:09 +1100

    Merge branch 'master' of https://github.com/churichchu/INFS3634
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/com/example/richard/assessment/QuizMainActivity.java

--- app/src/main/java/com/example/richard/assessment/QuizMainActivity.java
+++ app/src/main/java/com/example/richard/assessment/QuizMainActivity.java
@@ -62,10 +62,17 @@ public class QuizMainActivity extends AppCompatActivity {
         //IF statement to make sure random generated answers
         //are not the same as the correct answer nor previously
         //generated ones
-        if(randAnswer.equals(randAnswer) || randAnswer.equals(answerText)){
+        for(int i = 0; i <btns.length; i++){
             mRandAnswer = r2.nextInt(4-1)+1;
             randAnswer = am.get(mRandAnswer).getmAnswers();
+            if(!btns[i].getText().equals("Button")) {
+                i--;
+
+            }
+            btns[mRandAnswer].setText(randAnswer);
+
         }
+
 
         TextView tv = findViewById(R.id.questionText);
         tv.setText(questionText);
@@ -73,24 +80,24 @@ public class QuizMainActivity extends AppCompatActivity {
         Button btn = btns[mQnANum];
         btn.setText(answerText);
 
-        /*int ii = 0;
 
+        /*Button randBtn = btns[btnPlacementNum];
         for(int i = 1; i < btns.length; i++) {
-            Button randBtn = btns[btnPlacementNum];
 
             if (btns[btnPlacementNum].getText().equals("Button")) {
                 randBtn.setText(randAnswer);
                 takenAnswers.add(randAnswer);
-
-                ii++;
-            }else if(btns[btnPlacementNum].getText().equals(takenAnswers.get(ii))){
-                btnPlacementNum = r3.nextInt(4);
-                randBtn = btns[btnPlacementNum];
             }
             else{
 
                 btnPlacementNum = r3.nextInt(4);
                 i--;
+
+            }
+        }
+
+        for(int i = 0; i < btns.length; i++){
+            if(takenAnswers.contains(randAnswer)){
 
             }
         }*/
