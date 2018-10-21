4c7518a758069383cc1add5945fde2d613664e54
LiamM97 <liammalvern@gmail.com>
2018-10-21 21:29:03 +1100

    Merge branch 'master' of https://github.com/churichchu/INFS3634
    # Conflicts:
    #	app/src/main/java/com/example/richard/assessment/QuizMainActivity.java

--- app/src/main/java/com/example/richard/assessment/QuizMainActivity.java
+++ app/src/main/java/com/example/richard/assessment/QuizMainActivity.java
@@ -19,7 +19,20 @@ public class QuizMainActivity extends AppCompatActivity {
 
     int questionSelect;
     final int NUM_ANSWERS = 4;
-    String answerText;
+    String answerText, questionText, randAnswer;
+    TextView question;
+    int mQnANum, btnPlacementNum, mRandAnswer;
+    final Button[] btns = new Button[NUM_ANSWERS];
+
+    ArrayList<QuestionsModel> qm = new ArrayList<>();
+    ArrayList<AnswersModel> am = new ArrayList<>();
+    ArrayList<Integer> takenQ = new ArrayList<Integer>();
+
+    //Random Number generator for Questions and Answers
+    //r2 is meant for generating random incorrect answers when making mcq
+    Random r = new Random();
+    Random r2 = new Random();
+    Random r3 = new Random();
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -27,82 +40,93 @@ public class QuizMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_activity_main);
 
-        //retrieved questions array list
-        ArrayList<QuestionsModel> qm = new ArrayList<>();
-        qm = QandADatabase.getQuestionsArrayList();
-
-        //retrieved answers array list
-        ArrayList<AnswersModel> am = new ArrayList<>();
-        am = QandADatabase.getAnswersArrayList();
-
-        //Random Number generator for Questions and Answers
-        //r2 is meant for generating random incorrect answers when making mcq
-
-        Random r = new Random();
-        Random r2 = new Random();
-        Random r3 = new Random();
-
-        int mQnANum = r.nextInt(3 - 0 + 1) + 0;
-        int btnPlacementNum = r2.nextInt(3 - 0 + 1) + 0;
-
-
-        //IF statement so that random mcq answers cannot
-        //be placed int he same box as the correct answer
-        if (btnPlacementNum == mQnANum) {
-            btnPlacementNum = r3.nextInt(4);
-        }
+        question = findViewById(R.id.questionText);
 
         //Buttons array to randomly place answers for generated questions
-        final Button[] btns = new Button[4];
         btns[0] = (Button) findViewById(R.id.btn1);
         btns[1] = (Button) findViewById(R.id.btn2);
         btns[2] = (Button) findViewById(R.id.btn3);
         btns[3] = (Button) findViewById(R.id.btn4);
 
-        String questionText = qm.get(mQnANum).getmQuestion();
-        answerText = am.get(mQnANum).getmAnswers();
-        String randAnswer;
+        //retrieved questions array list
 
-        Button btn = btns[mQnANum];
-        btn.setText(answerText);
+        qm = QandADatabase.getQuestionsArrayList();
 
-        TextView tv = findViewById(R.id.questionText);
-        tv.setText(questionText);
+        //retrieved answers array list
 
-        //populate answers
-        ArrayList<Integer> randNums = new ArrayList<Integer>();
-        btns[mQnANum].setText(answerText);
-        randNums.add(mQnANum);
+        am = QandADatabase.getAnswersArrayList();
 
-        //IF statement to make sure random generated answers
-        //are not the same as the correct answer nor previously
-        //generated ones
-        int mRandAnswer = 0;
-        for(int i = 0; i < 20; i++) {
-            mRandAnswer = r.nextInt(3 - 0 + 1) + 0;
-            if (!btns[mRandAnswer].equals(answerText) && !randNums.contains(mRandAnswer)) {
-                randAnswer = am.get(mRandAnswer).getmAnswers();
-                btns[mRandAnswer].setText(randAnswer);
-                randNums.add(mRandAnswer);
-            }
-        }
+        for(int i = 0; i < NUM_ANSWERS; i++) {
+            mQnANum = r.nextInt((am.size() - 1)+ 1);
+            btnPlacementNum = r2.nextInt((NUM_ANSWERS -1)+ 1);
 
-        for(int k = 0; k < btns.length; k++) {
-            final int finalK = k;
-            btns[k].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(btns[finalK].getText().toString().equals(answerText)) {
-                        createDialogBox().setMessage("Correct!").show();
-                    } else {
-                        createDialogBox().setMessage("Incorrect!").show();
+            //IF statement so that random mcq answers cannot
+            //be placed int he same box as the correct answer
+
+
+            if(i == 0) {
+                if (btnPlacementNum == mQnANum) {
+                    btnPlacementNum = r3.nextInt(4);
+                }
+                questionText = qm.get(mQnANum).getmQuestion();
+                answerText = am.get(mQnANum).getmAnswers();
+                btns[btnPlacementNum].setText(answerText);
+                takenQ.add(mQnANum);
+
+                //IF statement to make sure random generated answers
+                //are not the same as the correct answer nor previously
+                //generated ones
+                for(int j = 0; j < 20; j++) {
+                    if (btnPlacementNum == mQnANum) {
+                        btnPlacementNum = r3.nextInt(4);
                     }
-                    for (int i = 0; i < btns.length; i++) {
-                        btns[i].setEnabled(false);
-
+                    mRandAnswer = r.nextInt(3 - 0 + 1) + 0;
+                    if (!btns[mRandAnswer].equals(answerText) && !takenQ.contains(mRandAnswer)) {
+                        randAnswer = am.get(mRandAnswer).getmAnswers();
+                        btns[mRandAnswer].setText(randAnswer);
+                        takenQ.add(mRandAnswer);
                     }
                 }
-            });
+            } else {
+                mQnANum = r.nextInt((am.size() - 1)+ 1);
+                if(!takenQ.contains(mQnANum)) {
+                    questionText = qm.get(mQnANum).getmQuestion();
+                    answerText = am.get(mQnANum).getmAnswers();
+                    btns[btnPlacementNum].setText(answerText);
+                    takenQ.add(mQnANum);
+                    for(int j = 0; j < 20; j++) {
+                        if (btnPlacementNum == mQnANum) {
+                            btnPlacementNum = r3.nextInt(4);
+                        }
+                        mRandAnswer = r.nextInt(3 - 0 + 1) + 0;
+                        if (!btns[mRandAnswer].equals(answerText) && !takenQ.contains(mRandAnswer)) {
+                            randAnswer = am.get(mRandAnswer).getmAnswers();
+                            btns[mRandAnswer].setText(randAnswer);
+                            takenQ.add(mRandAnswer);
+                        }
+                    }
+                } else {
+                    mQnANum = r.nextInt((am.size() - 1)+ 1);
+                }
+            }
+
+            for(int k = 0; k < btns.length; k++) {
+                final int finalK = k;
+                btns[k].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(btns[finalK].getText().toString().equals(answerText)) {
+                            createDialogBox().setMessage("Correct!").show();
+                        } else {
+                            createDialogBox().setMessage("Incorrect!").show();
+                        }
+                        for (int i = 0; i < btns.length; i++) {
+                            btns[i].setEnabled(false);
+
+                        }
+                    }
+                });
+            }
         }
 
     }
@@ -112,9 +136,12 @@ public class QuizMainActivity extends AppCompatActivity {
         ansFeedback.setTitle("Feedback");
         ansFeedback.setCancelable(false);
         ansFeedback.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //go to next question
+                for(int i = 0; i < NUM_ANSWERS; i++) {
+
+                }
             }
         });
         return ansFeedback;
